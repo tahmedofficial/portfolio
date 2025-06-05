457eb20d8ce8aca4de19ed68ee9a0adb1d76164e
tahmedofficial <tanvirofcuo@gmail.com>
2025-06-05 16:37:52 +0600

    bug fixing

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,5 +1,7 @@
 "use client";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import Cookies from "js-cookie";
+
 
 interface ModalProps {
     isOpen: boolean;
@@ -9,14 +11,24 @@ interface ModalProps {
 const Modal: FC<ModalProps> = ({ isOpen }) => {
     if (!isOpen) return null;
 
+    const [theme, setTheme] = useState("light");
+
+    useEffect(() => {
+        const storedTheme = Cookies.get("theme")
+        if (storedTheme) {
+            setTheme(storedTheme);
+        }
+    }, [])
+
+
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center" role="dialog" aria-hidden={!isOpen}>
+        <div className="fixed inset-0 bg-transparent bg-opacity-30 backdrop-blur-sm flex justify-center items-center" role="dialog" aria-hidden={!isOpen}>
             <div
-                className="bg-white rounded-xl px-20 py-10 flex flex-col gap-5 items-center mx-4"
+                className={`${theme == "light" ? "bg-white text-black" : "bg-black text-white"} rounded-xl px-20 py-10 flex flex-col gap-5 items-center mx-4`}
                 onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside
             >
-                <h1 className="text-2xl md:text-3xl font-medium text-black">Sending message</h1>
-                <span className="loading text-black loading-dots loading-md"></span>
+                <h1 className="text-2xl md:text-3xl font-medium">Sending message</h1>
+                <span className="loading loading-dots loading-md"></span>
             </div>
         </div>
     );
